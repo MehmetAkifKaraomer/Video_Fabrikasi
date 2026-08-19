fc5931a58dd0f77a1701e2fc81cef7696f03b76a
Akif <ertugrulkaraomerr@gmail.com>
2026-08-19 05:08:30 +0300

    gereksiz test.py silindi, __pycache__/ gitignore'a eklendi

--- scenes/test.py
+++
@@ -1,6 +0,0 @@
-from manim import *
-class Test(Scene):
-    def construct(self):
-        self.camera.background_color = "#0B0B0F"
-        self.play(Write(Text("Merhaba", color="#F5F5F5")))
-        self.wait(1)
